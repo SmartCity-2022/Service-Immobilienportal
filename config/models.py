1bc9bc983eb76b2f15f2cbc0753cbdc50b7edf35
Fabian <fabian.pechta@gmx.de>
2022-05-21 17:25:59 +0200

    Added models and schemas

--- config/models.py
+++ config/models.py
@@ -1,13 +1,55 @@
 from .db import base
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class Immobilien(base):
     __tablename__ = 'immobilien'
     id = Column(Integer, primary_key=True, index=True)
+    makler_id = Column(Integer, ForeignKey('immobilienmakler.id'))
     address = Column(String(255))
     zipcode = Column(String(255))
     city = Column(String(255))
     price = Column(Integer)
     created_at = Column(DateTime)
     updated_at = Column(DateTime)
+
+    makler = relationship("Immobilienmakler", back_populates="immobilien")
+
+class Immobilienmakler(base):
+    __tablename__ = 'immobilienmakler'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    telnr = Column(String(255))
+    hashed_password = Column(String(255))
+    surname = Column(String(255))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    immobilien = relationship('Immobilien', back_populates='makler')
+    termin = relationship('Termin', back_populates='immobilienmakler')
+
+class User(base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255))
+    surname = Column(String(255))
+    hashed_password = Column(String(255))
+    telnr = Column(String(255))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+    termin = relationship('Termin', back_populates='user')
+
+class Termin(base):
+    __tablename__ = 'termin'
+    id = Column(Integer, primary_key=True, index=True)
+    termin_art = Column(String(255))
+    termin_date = Column(DateTime)
+    termin_dauer = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    immobilienmakler_id = Column(Integer, ForeignKey('immobilienmakler.id'))
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+    
+    makler_termin = relationship('Immobilienmakler', back_populates='termin')
+    user_termin = relationship('User', back_populates='termin')
--- config/schemas.py
+++ config/schemas.py
@@ -9,3 +9,35 @@ class ImmobilienSchema(BaseModel):
     price: int
     created_at: datetime
     updated_at: datetime
+    
+class TerminSchema(BaseModel):
+    termin_art: str
+    termin_date: datetime
+    termin_dauer: int
+    user_id: int
+    immobilienmakler_id: int
+    created_at: datetime
+    updated_at: datetime
+
+class ImmobilienmaklerSchema(BaseModel):
+    name: str
+    telnr: int
+    hashed_password: str
+    surname: str
+    created_at: datetime
+    updated_at: datetime
+    immobilien = list[ImmobilienSchema] = []
+    termin = list[TerminSchema] = []
+
+class UserSchema(BaseModel):
+    name: str
+    surname: str
+    hashed_password: str
+    telnr: int
+    created_at: datetime
+    updated_at: datetime
+    termin = list[TerminSchema] = []
+
+class TerminCreateSchema(TerminSchema):
+    makler_termin = list[TerminSchema] = []
+    user_termin = list[TerminSchema] = []
