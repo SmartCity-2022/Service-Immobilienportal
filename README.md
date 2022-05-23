7399ff2a37c4d060fb9c9a511581318aca8054dc
Fabian <fabian.pechta@gmx.de>
2022-05-24 01:14:11 +0200

    Fixes, added a .env for db url and readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Service-Immobilienportal
 Microservice Immobilienportal
+
+.env
+DB_URL="mysql://root@localhost:3307/immobilienportal"
--- config/db.py
+++ config/db.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import find_dotenv, load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql://root@localhost:3307/immobilienportal"
+load_dotenv(find_dotenv())
+url = os.getenv('DB_URL')
+
+SQLALCHEMY_DATABASE_URL = url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
